f0d453d4c6f553b3ab6f74dadeac8ef6b04b54c6
aamert <46253116+aamert@users.noreply.github.com>
2020-11-08 11:10:53 -0600

    Update Finance_To_Csv.py

--- Data/Finance_To_Csv.py
+++ Data/Finance_To_Csv.py
@@ -1,29 +1,79 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov  3 23:31:15 2020
+Created on Sat Nov  7 00:30:00 2020
 
 @author: JTSC
 """
-#Imports
+
+import pathlib
+import os.path
+import time
+from datetime import date
 import yfinance as yf
-#import pandas as pd
-#import sqlite3
-#import csv
-#import plotly_express as px
 
-    #User Provided Stock Symbol
-userInput = input().upper()
+#Users Input for Stock Symbol
+def Users_input():
+    return input()
 
+#Sets Users Input
+symbol = Users_input()
+
+#Sets Filepath For Current File
+path = os.path.dirname(__file__)
+
+
+#Returns the final requested stocks filepath
+def Stock_Filepath():
+    filepath = (path + "\{}.csv").format(symbol) 
+    return str(filepath)
+
+#Sets Stock Filepath with Attached CSV
+filepath = Stock_Filepath()
+
+  
+#Checks if Stock File Exists
+def Stock_exists(): 
+    #Checking to see if filepath Exists
+    if pathlib.Path(filepath).exists() == True:
+        
+        #If Filepath Exists Check Last Date File Was updated, If today return 1 else 0
+        if time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(filepath))) == str(date.today()):
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+#Sets result answer to 1:0 based on Stock_exists( function)   
+Stock_Existing_Test = Stock_exists()
+    
+#Used to download stock information    
+def csv_download():
     #Gets Stock Symbol Information
-tickers = yf.Ticker(userInput)
+    tickers = yf.Ticker(symbol)
 
     #Generates Max Range of Data
-hist = tickers.history(period="max").reset_index()
+    hist = tickers.history(period="max").reset_index()
 
     #Creates a CSV filename Based on User Input
-fileName = "{}.csv".format(userInput)
+    fileName = "{}.csv".format(symbol)
 
     #Uses Filename to Create CSV File
-hist.to_csv(fileName,
+    hist.to_csv(fileName,
             index=False,
             sep=",")
+    return print("Downloaded")
+    
+
+    #Checks to see if Stock Symbol CSV Exists
+def file_check():        
+    #stock_exists checks if file exists - if it does, Checks if file was updated today.
+    if Stock_Existing_Test == 1:
+        print("File Up To Date")
+        
+    #If Stock doesn't exists, it gets downloaded
+    else:
+        csv_download()
+
+
+#Set for file initialization in spyder
+file_check() 
