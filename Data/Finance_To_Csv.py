35b4c0bad1965b852bfa02bd2390e01df366d028
aamert <46253116+aamert@users.noreply.github.com>
2020-11-12 22:36:45 -0600

    Updated Finance to csv
    Updated to handle bad symbol requests

--- Data/Finance_To_Csv.py
+++ Data/Finance_To_Csv.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov  9 00:30:00 2020
+Created on Thu Nov 12th 10:32:00.00.00
 
 @author: JTSC
 """
@@ -11,27 +11,27 @@ import time
 from datetime import date
 import yfinance as yf
 
+path = os.path.dirname(__file__)
+#############################################################
 #Users Input for Stock Symbol
+#############################################################
 def Users_input():
-    return input()
+    symbol = input()
+    return symbol
 
+#############################################################
 #Sets Users Input
 symbol = Users_input().upper()
+#############################################################
 
-#Sets Filepath For Current File
-path = os.path.dirname(__file__)
+#############################################################
+#Sets Filepath For inputted csv
+filepath = (path + "\{}.csv").format(symbol)
+#############################################################
 
-
-#Returns the final requested stocks filepath
-def Stock_Filepath():
-    filepath = (path + "\{}.csv").format(symbol) 
-    return str(filepath)
-
-#Sets Stock Filepath with Attached CSV
-filepath = Stock_Filepath()
-
-  
+#############################################################
 #Checks if Stock File Exists
+#############################################################
 def Stock_exists(): 
     #Checking to see if filepath Exists
     if pathlib.Path(filepath).exists() == True:
@@ -43,44 +43,37 @@ def Stock_exists():
             return 0
     else:
         return 0
-#Sets result answer to 1:0 based on Stock_exists( function)   
-Stock_Existing_Test = Stock_exists()
-    
-#Used to download stock information    
-def csv_download():
-    #Gets Stock Symbol Information
-    tickers = yf.Ticker(symbol)
-
-    #Generates Max Range of Data
-    hist = tickers.history(period="max").reset_index()
-    #Causes an exception error: This is needed in order to not download csv files with no data due to invalid stock symbols E.G. (TSLA would be excepted, dushoqysh would not be)
-    try:
-        if hist.empty:
-            print("Invalid Symbol - Please Try Again")
-    except:
-        print("data is empty")
-        raise Exception
-    else:
-        pass
-
-    #Creates a CSV filename Based on User Input
-    fileName = "{}.csv".format(symbol)
-
-    #Uses Filename to Create CSV File
-    hist.to_csv(fileName,
+#############################################################
+#Tries Inputted stock symbol to see if it's a valid symbol
+#############################################################
+def Try_Symbol():
+    #Takes inputed symbol and checks if it's valid
+     tickers = yf.Ticker(symbol)
+     try:
+        hist = tickers.history(period="max").reset_index()
+        #If Hist is an empty dataframe, Raises error otherwise if valid and doesn't already exists, gets downloaded
+        if hist.empty == True:
+           raise 
+        else:
+                pass
+     except:
+        print("Try Again - Enter Valid Symbol")
+     else:
+         if Stock_exists() == 0:
+        #1 means successful attempt at symbol set
+             hist.to_csv(filepath,
             index=False,
-            sep=",")
-    return print("Downloaded")
-    
-
+            sep=",") ,print("downloaded") 
+         else: 
+             pass
+#############################################################
     #Checks to see if Stock Symbol CSV Exists
+#############################################################
 def file_check():        
-    #stock_exists checks if file exists - if it does, Checks if file was updated today.
-    if Stock_Existing_Test == 1:
-      return   print(("{}.csv".format(symbol)))
-        
+    if Stock_exists() == 1:
+        return print(("{}.csv".format(symbol)))
     #If Stock doesn't exists, it gets downloaded
     else:
-       return csv_download()
-   
+       return Try_Symbol()
+ 
 file_check()
